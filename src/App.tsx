216905a3d9be1ba4c73556efaec445614b794e3d
shyamdhokia1 <shyamdhokia123@hotmail.co.uk>
2024-08-23 15:30:31 +0000

    feat: fetch and search button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
-import { useEffect, useState } from "react";
+import { useEffect, useState, ChangeEvent } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
+import { searchProducts, Product } from "./functions/searchProducts";
 
 function App() {
   const [count, setCount] = useState(0);
   const [clockOn, setClockOn] = useState(true);
+  const [search, setSearch] = useState("");
 
   useEffect(() => {
     const timer = setInterval(() => {
@@ -13,7 +15,11 @@ function App() {
       }
     }, 2000);
     return () => clearInterval(timer);
-  },[clockOn]);
+  }, [clockOn]);
+
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearch(event.target.value);
+  };
 
   return (
     <>
@@ -25,16 +31,24 @@ function App() {
       <h1>Eolas Test</h1>
       <div className="card">
         <div className="top-buttons">
-          <button className = "small-button" onClick={() => setCount((count) => count + 1)}>
+          <button className="small-button" onClick={() => setCount((count) => count + 1)}>
             Count is {count}
           </button>
-          <button className = "small-button" onClick={() => setClockOn((clockOn)=> clockOn ? false : true)}>
+          <button className="small-button" onClick={() => setClockOn((clockOn) => clockOn ? false : true)}>
             {clockOn ? 'Pause' : 'Resume'}
           </button>
         </div>
-        <button className="large-button" onClick={()=> {setClockOn(false); setCount(0);}}>
+        <div>
+          <button className="reset-button" onClick={() => { setClockOn(false); setCount(0); }}>
             Reset
-        </button>
+          </button>
+        </div>
+        <div>
+          <input className="search-input" type="text" placeholder="Search for Products" onChange={handleChange}></input>
+          <button className="search-button" onClick={() => searchProducts(search)}>
+            Search
+          </button>
+        </div>
       </div>
     </>
   );
--- src/functions/searchProducts.ts
+++ src/functions/searchProducts.ts
@@ -1,5 +1,52 @@
-export type Product = {};
+export type Product = {
+  "id": number,
+  "title": string,
+  "description": string,
+  "category": string,
+  "price": number,
+  "discountPercentage": number,
+  "rating": number,
+  "stock": number,
+  "tags": string[],
+  "sku": string,
+  "weight": number,
+  "dimensions": {
+    "width": number,
+    "height": number,
+    "depth": number
+  },
+  "warrantyInformation": string,
+  "shippingInformation": string,
+  "availabilityStatus": string,
+  "reviews": Review[],
+  "returnPolicy": string,
+  "minimumOrderQuantity": number,
+  "meta": {
+    "createdAt": string,
+    "updatedAt": string,
+    "barcode": string,
+    "qrCode": string,
+  },
+  "images": string[],
+  "thumbnail": string,
+};
+
+type Review = {
+  "rating": number,
+  "comment": string,
+  "date": string,
+  "reviewerName": string,
+  "reviewerEmail": string
+};
+
 
 export const searchProducts = async (queryString: string) => {
-  return [] as Product[];
+  let url: string = "https://dummyjson.com/products"
+  if (queryString) {
+    url = `https://dummyjson.com/products/search?q=${queryString}`
+  }
+  const res = await fetch(url);
+  const items = await res.json();
+  console.log(items.products);
+  return items.products as Product[];
 };
